2ee6726d1d39d4e669ecd8d64c27dbed09065797
spyridon-amp <sampanavos@gsd.harvard.edu>
2019-10-20 23:57:22 -0400

    fixed data format, communication established

--- Simulated Annealing for Grasshopper/Program.cs
+++ Simulated Annealing for Grasshopper/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,6 +14,34 @@ namespace Simulated_Annealing_for_Grasshopper
             SimulatedAnnealing simulatedAnnealing = new SimulatedAnnealing();
             double best = simulatedAnnealing.Run();
             Console.WriteLine(format: "optimal result: {0}", arg0: best.ToString());
+
+            string outMessage = JsonConvert.SerializeObject(new DataExchange(best, 0, 0));
+            string response = SynchronousClientSocket.RequestResponse(11000, outMessage);
+            Console.WriteLine(response);
+
+            try
+            {
+                DataExchange incoming = JsonConvert.DeserializeObject<DataExchange>(response);
+                Console.WriteLine(incoming.value.ToString());
+            }
+            catch
+            {
+                Console.WriteLine( "Unexpected data format received");
+            }
+        }
+    }
+
+    class DataExchange
+    {
+        public double value { get; set; }
+        public double record1 { get; set; }
+        public double record2 { get; set; }
+
+        public DataExchange(double v, double r1, double r2)
+        {
+            value = v;
+            record1 = r1;
+            record2 = r2;
         }
     }
 }
---
+++ Simulated Annealing for Grasshopper/SynchronousClientSocket.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace Simulated_Annealing_for_Grasshopper
+{
+    //adjusted from https://docs.microsoft.com/en-us/dotnet/framework/network-programming/synchronous-client-socket-example
+    class SynchronousClientSocket
+    {
+        public static string RequestResponse(Int32 port, string message)
+        {
+            // Data buffer for incoming data.  
+            byte[] bytes = new byte[1024];
+
+            // Connect to a remote device.  
+            try
+            {
+                // Establish the remote endpoint for the socket.  
+                // This example uses port 11000 on the local computer.  
+                IPHostEntry ipHostInfo = Dns.GetHostEntry(Dns.GetHostName());
+                IPAddress ipAddress = ipHostInfo.AddressList[0];
+                IPEndPoint remoteEP = new IPEndPoint(ipAddress, port);
+
+                // Create a TCP/IP  socket.  
+                Socket sender = new Socket(ipAddress.AddressFamily,
+                    SocketType.Stream, ProtocolType.Tcp);
+
+                // Connect the socket to the remote endpoint. Catch any errors.  
+                try
+                {
+                    sender.Connect(remoteEP);
+
+                    Console.WriteLine("Socket connected to {0}",
+                        sender.RemoteEndPoint.ToString());
+
+                    // Encode the data string into a byte array.  
+                    byte[] msg = Encoding.ASCII.GetBytes(message + "<EOF>"); 
+
+                    // Send the data through the socket.  
+                    int bytesSent = sender.Send(msg);
+
+                    // Receive the response from the remote device.  
+                    int bytesRec = sender.Receive(bytes);
+                    string IncomingMessage = Encoding.ASCII.GetString(bytes, 0, bytesRec);
+                    Console.WriteLine("Received = {0}", IncomingMessage);
+
+                    // Release the socket.  
+                    sender.Shutdown(SocketShutdown.Both);
+                    sender.Close();
+
+                    return IncomingMessage;
+                }
+                catch (ArgumentNullException ane)
+                {
+                    Console.WriteLine("ArgumentNullException : {0}", ane.ToString());
+                }
+                catch (SocketException se)
+                {
+                    Console.WriteLine("SocketException : {0}", se.ToString());
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Unexpected exception : {0}", e.ToString());
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+            return null;
+        }
+    }
+}
